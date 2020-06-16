539f9af71a4f10fc3748cad02aeefa2075cc43b1
occiandiaali <ocean@localhost.localdomain>
2020-06-16 18:32:31 +0100

    first commit

---
+++ app/src/main/java/com/occian/not3s/Note.kt
@@ -0,0 +1,7 @@
+package com.occian.not3s
+
+class Note(nodeID: Int, nodeName: String, nodeDesc: String) {
+    var nodeID: Int? = nodeID
+    var nodeName: String? = nodeName
+    var nodeDesc: String? = nodeDesc
+} // class
